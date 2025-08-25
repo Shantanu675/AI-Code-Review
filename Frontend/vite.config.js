2d74040e18715430cebc787f6e9341c016554ed0
Shantanu675 <shantanudanej05@gmail.com>
2025-08-26 01:37:38 +0530

    changes

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // or whatever plugins you're using
+
 export default defineConfig({
   plugins: [react()],
+  // Add or merge with existing build config
+  build: {
+    chunkSizeWarningLimit: 1000 // Increase to 1000 KB
+  },
+  // ... other existing config
 })
