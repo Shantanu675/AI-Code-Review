2e918cb9ce124c6965e742845367e38eaf7e58f9
Shantanu675 <shantanudanej05@gmail.com>
2025-08-26 01:20:17 +0530

    some changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
 
   async function review_code() {
     try {
-      const response = await axios.post('http://localhost:3000/ai/get-review',{code})
+      const response = await axios.post('https://ai-code-review-yvup.onrender.com/ai/get-review',{code})
       console.log(response.data);
       setReview(response.data);
     } catch (error) {
